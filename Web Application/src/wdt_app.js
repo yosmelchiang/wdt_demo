294866d9c9905719fee0f11d93e9e7e8bb9bcbec
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2024-11-19 13:16:38 +0100

    Refactoring and improved the time format for out logged cells

--- Web Application/src/wdt_app.js
+++ Web Application/src/wdt_app.js
@@ -82,42 +82,36 @@ function rowSelection() {
   }
 }
 
-
 /**
- * Date and time
-*/
-let d = new Date();
+ * Date, time and button handlers
+ */
+function addZero(i) {
+  if (i < 10) {
+    i = 0 + i;
+  }
+  return i;
+}
+const d = new Date();
+let hh = addZero(d.getHours());
+let mm = addZero(d.getMinutes());
 
 outButton.addEventListener('click', function () {
   const status = document.getElementsByClassName('rowSelection');
-  status[0].cells[4].innerHTML = 'Out';
-  let loggedOutTime = hh + ':' + mm
-  status[0].cells[5].innerHTML = loggedOutTime;
+  if (status.length > 0) {
+    status[0].cells[4].innerHTML = 'Out';
+    let loggedOutTime = hh + ':' + mm;
+    status[0].cells[5].innerHTML = loggedOutTime;
+  }
 });
 
 inButton.addEventListener('click', function () {
   const status = document.getElementsByClassName('rowSelection');
-  status[0].cells[4].innerHTML = 'In';
+  if (status.length > 0) {
+    status[0].cells[4].innerHTML = 'In';
+  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
 // let date = `${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}`
-
-// let hh = d.getHours();
-// console.log('hh:', hh)
-
-// let mm = d.getMinutes();
-// console.log('mm:', mm)
 
 // let ss = d.getSeconds();
 // console.log('ss:', ss)
