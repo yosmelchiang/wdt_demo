08718209109cc13aedf957aecbc9fbb390c05eef
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2024-12-01 13:39:50 +0100

    Implemented destructuring of fetch dat a results

--- Web Application/src/api/wdt_api.js
+++ Web Application/src/api/wdt_api.js
@@ -12,23 +12,44 @@ export function staffUserGet() {
   return fetch('https://randomuser.me/api/?results=5&seed=wdttm')
     .then((response) => response.json())
     .then((data) => {
-      const users = data.results;
 
-      for (let i = 0; i < users.length; i++) {
-        
-        const jsObject = { //Creating an Object for each fetched user of our API call
-          picture: users[i].picture.medium,
-          name: users[i].name.first,
-          surname: users[i].name.last,
-          email: users[i].email
-        };
+      // Learning how to destructure data
+      for (const { name, picture, email } of data.results) {
 
-        const staffInstance = factory.createEmployee('staff', jsObject)
-        
+        const { first, last } = name
+        const { medium } = picture
+
+        const JSObj = {
+          picture: medium,
+          name: first,
+          surname: last,
+          email: email
+        }
+
+        const staffInstance = factory.createEmployee('staff', JSObj)        
         const staffID = staffInstance.id;
-
         staffs[staffID] = staffInstance
       }
+
+
+      // const users = data.results;
+
+      // for (let i = 0; i < users.length; i++) {
+        
+      //   const jsObject = { //Creating an Object for each fetched user of our API call
+      //     picture: users[i].picture.medium,
+      //     name: users[i].name.first,
+      //     surname: users[i].name.last,
+      //     email: users[i].email
+      //   };
+        
+      //   const staffInstance = factory.createEmployee('staff', jsObject)
+        
+      //   const staffID = staffInstance.id;
+
+      //   staffs[staffID] = staffInstance
+      // }
+      console.log(staffs)
       return staffs;
     })
     .catch((error) => console.log('Error fetching users: ', error));
